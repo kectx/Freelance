1692fdf54b0282b15b50ee1f157473db0130e94f
kectx <xtecek@gmail.com>
2022-03-25 02:12:08 +0100

    .

--- Project1/AppsScript/Kod.js
+++ Project1/AppsScript/Kod.js
@@ -3,13 +3,12 @@ let sheetTask2 = ss.getSheetByName("Zadanie 2")
 let sheetTask3 = ss.getSheetByName("Zadanie 3")
 
 function myFunction() {
-  let range = sheetTask3.getDataRange();
+	let range = sheetTask3.getDataRange()
 	let lastRow = sheetTask3.getLastRow()
-  let i;
+	let i
 
-  for(i = 0; i < range.length ; i++){
-
-	if (sheetTask3.getRange(lastRow, 1) !== "")
-		sheetTask3.getRange(lastRow, 1).setValue("TEST123")
-  }
+	for (i = 0; i < range.length; i++) {
+		if (sheetTask3.getRange(lastRow, 1) !== "")
+			sheetTask3.getRange(lastRow, 1).setValue("TEST123")
+	}
 }
