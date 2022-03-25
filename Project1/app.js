6b0c935ad8e9efd65bdb1cb5e04e4c18b962474c
kectx <xtecek@gmail.com>
2022-03-25 02:00:52 +0100

    fast refresh

---
+++ Project1/app.js
@@ -0,0 +1,75 @@
+// const subSite1 = document.querySelector(".subsite1")
+// const subSite2 = document.querySelector(".subsite2")
+
+// subSite1.addEventListener("click", function (e) {
+// 	e.preventDefault()
+// 	if (subSite1) {
+// 		window.location.href =
+// 			subSite1.href +
+// 			"?przejscie" +
+// 			"?utm_source=Testy&utm_medium=Pierwsz&utm_campaign=Lato&utm_id=MateuszGr%C4%99da".replace(
+// 				"utm_campaign=Lato",
+// 				"utm_campaign=Zima"
+// 			)
+// 	}
+// })
+
+const changeURL = () => {
+	let path = window.location.pathname
+	console.log(path)
+	if (path === "/Project1/index.html") {
+		window.history.pushState(
+			null,
+			null,
+			"?utm_source=Testy&utm_medium=Pierwsz&utm_campaign=Lato&utm_id=MateuszGr%C4%99da"
+		)
+	} else if (path === "/Project1/ws1.html") {
+		window.history.pushState(
+			null,
+			null,
+			"#przejscie?utm_source=Testy&utm_medium=Pierwsz&utm_campaign=Lato&utm_id=MateuszGr%C4%99da".replace(
+				"utm_campaign=Lato",
+				"utm_campaign=Zima"
+			)
+		)
+	} else if (path === "/Project1/ws2.html") {
+		href = window.location.href
+	}
+}
+changeURL()
+
+// let href = window.location.href
+// window.addEventListener("onload", (href) => {
+// 	console.log(href)
+// 	if (href === "Project1/index.html") {
+// 		window.history.pushState(
+// 			"",
+// 			"",
+// 			"?utm_source=Testy&utm_medium=Pierwsz&utm_campaign=Lato&utm_id=MateuszGr%C4%99da"
+// 		)
+// 	} else if (href === "Project/ws1.html") {
+// 		window.history.pushState(
+// 			null,
+// 			null,
+// 			"?przejscie?utm_source=Testy&utm_medium=Pierwsz&utm_campaign=Zima&utm_id=MateuszGr%C4%99da"
+// 		)
+// 	} else if (href === "Project/ws2.html") {
+// 		href = window.location.href
+// 	}
+// })
+
+//
+function calcBMI(weight, height) {
+	let bmi = (weight / Math.pow(height / 100, 2)).toFixed(2)
+	if (bmi <= 18.5) {
+		console.log("Niedowaga")
+	} else if (bmi <= 25 && bmi > 18.5) {
+		console.log("Wartość prawidłowa")
+	} else {
+		console.log("Nadwaga")
+	}
+
+	return bmi
+}
+
+calcBMI(95, 190)
