9b3847038f883bd997c47030680164add26ff6b3
xtcek <mateusz.greda@tutore.eu>
2022-07-04 15:57:16 +0200

    Render Meals component on main page

--- React/react-course-section11/src/App.js
+++ React/react-course-section11/src/App.js
@@ -1,10 +1,15 @@
 import { Fragment } from "react"
 import Header from "./components/Layout/Header"
+import Meals from "./components/Meals/Meals"
+import Card from "./components/UI/Card"
 
 function App() {
 	return (
 		<Fragment>
 			<Header />
+			<main>
+				<Meals />
+			</main>
 		</Fragment>
 	)
 }
